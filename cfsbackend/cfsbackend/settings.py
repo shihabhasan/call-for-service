a68988a71adf699bb1325275bff908ff0d2e622d
Adam Preston <adam.m.preston@gmail.com>
2015-08-05 16:55:49 -0600

    allowing limit and offset for all APIs

--- cfsbackend/cfsbackend/settings.py
+++ cfsbackend/cfsbackend/settings.py
@@ -109,5 +109,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 1000
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
