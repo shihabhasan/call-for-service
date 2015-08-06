0d472265bf6f976dc00e28a3631778339fb7a3e2
Adam Preston <adam.m.preston@gmail.com>
2015-08-06 16:09:25 -0600

    not exactly authenticating anymore (for now)

--- cfsbackend/cfsbackend/settings.py
+++ cfsbackend/cfsbackend/settings.py
@@ -114,7 +114,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
     'PAGE_SIZE': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
