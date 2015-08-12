b131032e42eaa16415e92bfff2267617f47c2085
Jason Nance <jnance@rti.org>
2015-08-12 17:28:34 -0400

    Minor fix to in_call view

--- working_files/scripts/in_call.sql
+++ working_files/scripts/in_call.sql
@@ -1,7 +1,8 @@
 /*
-For each call, displays the start (dispatched) and end (cleared) times for each unit involved with the call, as well as the shift_unit_id of that unit.
+For each call, displays the start (dispatched) and end (cleared) times for each unit involved with the call, as well as the call_id, shift_unit_id, and
+call_unit_id of that unit.
 
-The view is materialized because it takes about 2 seconds to create.  This is inefficient when querying it multiple times.
+The view is materialized because it takes several seconds to create.  This is inefficient when querying it multiple times.
 */
 
  -- ensure these indexes exist or else it will be super slow
@@ -9,15 +10,20 @@ The view is materialized because it takes about 2 seconds to create.  This is in
  --CREATE INDEX call_log_call_id ON call_log (call_id);
  --CREATE INDEX call_log_call_unit_id ON call_log(call_unit_id);
 DROP MATERIALIZED VIEW IF EXISTS in_call CASCADE;
+
+
 CREATE MATERIALIZED VIEW in_call AS
+WITH
+start_ids AS (SELECT transaction_id FROM transaction WHERE descr = 'Dispatched'),
+end_ids AS (SELECT transaction_id FROM transaction WHERE descr in ('Cleared','Canceled'))
  SELECT c.call_id,
     start_.time_recorded AS start_time,
     end_.time_recorded AS end_time,
     start_.shift_unit_id,
     start_.call_unit_id AS start_call_unit_id,
     end_.call_unit_id AS end_call_unit_id
-   FROM call c,
-    ( SELECT cl1.call_log_id,
+   FROM call c, (
+       SELECT cl1.call_log_id,
             cl1.transaction_id,
             cl1.shift_unit_id,
             cl1.time_recorded,
@@ -25,8 +31,9 @@ CREATE MATERIALIZED VIEW in_call AS
             cl1.call_unit_id,
             cl1.close_code_id
            FROM call_log cl1
-          WHERE cl1.transaction_id = 115) start_,
-    ( SELECT cl2.call_log_id,
+          WHERE cl1.transaction_id IN (SELECT transaction_id FROM start_ids)
+    ) start_, (
+        SELECT cl2.call_log_id,
             cl2.transaction_id,
             cl2.shift_unit_id,
             cl2.time_recorded,
@@ -34,10 +41,11 @@ CREATE MATERIALIZED VIEW in_call AS
             cl2.call_unit_id,
             cl2.close_code_id
            FROM call_log cl2
-          WHERE cl2.transaction_id IN (384, 145)) end_
+          WHERE cl2.transaction_id IN (SELECT transaction_id FROM end_ids)
+    ) end_
   WHERE start_.call_id = c.call_id
     AND end_.call_id = c.call_id
-    AND start_.shift_unit_id = end_.shift_unit_id
+    AND start_.call_unit_id = end_.call_unit_id
     
     -- ensure our start is the closest dispatch to the clear/cancel
     AND start_.time_recorded = (
@@ -45,7 +53,7 @@ CREATE MATERIALIZED VIEW in_call AS
       FROM call_log cl_closest_start
       WHERE cl_closest_start.call_id = end_.call_id
         AND cl_closest_start.call_unit_id = end_.call_unit_id
-        AND cl_closest_start.transaction_id = 115
+        AND cl_closest_start.transaction_id IN (SELECT transaction_id FROM start_ids)
         AND cl_closest_start.time_recorded < end_.time_recorded
     /*
       -- this is slow
@@ -64,7 +72,7 @@ CREATE MATERIALIZED VIEW in_call AS
       FROM call_log cl_closest_end
       WHERE cl_closest_end.call_id = start_.call_id
         AND cl_closest_end.call_unit_id = start_.call_unit_id
-        AND cl_closest_end.transaction_id IN (145, 384)
+        AND cl_closest_end.transaction_id IN (SELECT transaction_id FROM end_ids)
         AND cl_closest_end.time_recorded > start_.time_recorded
     /*
     -- this is slow
