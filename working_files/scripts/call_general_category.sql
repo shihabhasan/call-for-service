a5297043e421246018093ed53ee76b47b5dc2725
Jason Nance <jnance@rti.org>
2015-08-26 11:24:57 -0400

    Add gang_related to call_general_category view
    Added additional regexes to identify gang-related calls based on
    their notes.  Note that these don't correlate especially well
    with the DPD's existing "gang_related" column in the incident
    table; many of the DPD-identified gang-related calls don't have
    anything that would indicate gang activity/affiliation in their
    notes.  Most of the regex-identified calls are calls on which
    a residence turns up a "suspected gang member" or "validated gang
    member" alert for one of its occupants.  I'm not sure how useful
    this category will be for the DPD because of that.

--- working_files/scripts/call_general_category.sql
+++ working_files/scripts/call_general_category.sql
@@ -1,22 +1,40 @@
+DROP MATERIALIZED VIEW IF EXISTS call_general_category;
+
 CREATE MATERIALIZED VIEW call_general_category AS
-WITH gun_ids AS (SELECT distinct call_id FROM note WHERE
-(
-(upper(body) LIKE '%GUN%' AND upper(body) ~ '\m(SHOT|HAND)?GUNS?\M(?! IS INVOLVED)') OR
-(upper(body) LIKE '%SHOTS HAVE BEEN FIRED%') OR
-(upper(body) LIKE '%RIFLE%' AND upper(body) ~ 'RIFLE(?!D)S?') OR
-(upper(body) LIKE '%GUN%' AND upper(body) ~ 'A GUN IS INVOLVED: .*(\.?22|9\s?MM|\.?45|\.?40)') OR
-(upper(body) LIKE '%REVOLVER%') OR
-(upper(body) LIKE '%PISTOL%')
+WITH gun_ids AS (
+  SELECT distinct call_id
+  FROM note
+  WHERE (
+    (upper(body) LIKE '%GUN%' AND upper(body) ~ '\m(SHOT|HAND)?GUNS?\M(?! IS INVOLVED)') OR
+    (upper(body) LIKE '%SHOTS HAVE BEEN FIRED%') OR
+    (upper(body) LIKE '%RIFLE%' AND upper(body) ~ 'RIFLE(?!D)S?') OR
+    (upper(body) LIKE '%GUN%' AND upper(body) ~ 'A GUN IS INVOLVED: .*(\.?22|9\s?MM|\.?45|\.?40)') OR
+    (upper(body) LIKE '%REVOLVER%') OR
+    (upper(body) LIKE '%PISTOL%')
+  )
+    AND NOT (
+    (upper(body) ~ 'NO (SHOT|HAND)?GUNS?') OR
+    (upper(body) ~ 'TOY (GUN|RIFLE|SHOTGUN|PISTOL|HANDGUN)')
+  )
+),
+gang_ids AS (
+  SELECT distinct call_id
+  FROM note
+  WHERE (
+    (upper(body) LIKE '%GANG%' AND upper(body) ~ '\mGANG') OR
+    (upper(body) LIKE '%CRIP%' AND upper(body) ~ '\mCRIP\M') OR
+    (upper(body) LIKE '%BLOODS%') OR
+    (upper(body) LIKE '%MS13%')
+  )
 )
-AND NOT
-(
-(upper(body) ~ 'NO (SHOT|HAND)?GUNS?') OR
-(upper(body) ~ 'TOY (GUN|RIFLE|SHOTGUN|PISTOL|HANDGUN)')
-))
 SELECT
   call_id,
   CASE
     WHEN call_id IN (SELECT * FROM gun_ids) THEN TRUE
     ELSE FALSE
-  END AS gun_related
+  END AS gun_related,
+  CASE
+    WHEN call_id IN (SELECT * FROM gang_ids) THEN TRUE
+    ELSE FALSE
+   END AS gang_related
 FROM call;
