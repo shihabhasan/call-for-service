8faa8c6483d2e08c076e6e3292d3d221e8826e8c
Jason Nance <jnance@rti.org>
2015-08-26 10:02:37 -0400

    Add gun_related to call_general_category view
    The materialized view uses a set of regexes to determine whether
    calls appear to be gun-related or not.  This was based on similar
    categorization from the Tampa project, but some additions were made.

--- working_files/scripts/general_categories.sql
+++ working_files/scripts/general_categories.sql
@@ -1,7 +1,10 @@
-SELECT upper(body) FROM note WHERE
+CREATE MATERIALIZED VIEW call_general_category AS
+WITH gun_ids AS (SELECT distinct call_id FROM note WHERE
 (
-(upper(body) LIKE '%GUN%' AND upper(body) ~ '\m(SHOT|HAND)?GUNS?\M') OR
-(upper(body) LIKE '%RIFLE%' AND upper(body) ~ 'RIFLES?') OR
+(upper(body) LIKE '%GUN%' AND upper(body) ~ '\m(SHOT|HAND)?GUNS?\M(?! IS INVOLVED)') OR
+(upper(body) LIKE '%SHOTS HAVE BEEN FIRED%') OR
+(upper(body) LIKE '%RIFLE%' AND upper(body) ~ 'RIFLE(?!D)S?') OR
+(upper(body) LIKE '%GUN%' AND upper(body) ~ 'A GUN IS INVOLVED: .*(\.?22|9\s?MM|\.?45|\.?40)') OR
 (upper(body) LIKE '%REVOLVER%') OR
 (upper(body) LIKE '%PISTOL%')
 )
@@ -9,4 +12,11 @@ AND NOT
 (
 (upper(body) ~ 'NO (SHOT|HAND)?GUNS?') OR
 (upper(body) ~ 'TOY (GUN|RIFLE|SHOTGUN|PISTOL|HANDGUN)')
-);
+))
+SELECT
+  call_id,
+  CASE
+    WHEN call_id IN (SELECT * FROM gun_ids) THEN TRUE
+    ELSE FALSE
+  END AS gun_related
+FROM call;
