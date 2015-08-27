70dd46ec8775151c5a584a9598548396ac1a67b4
Jason Nance <jnance@rti.org>
2015-08-27 14:30:35 -0400

    Add ofcr/ctzn conflict to general call categories
    We can use a regex to parse the call notes and identify calls
    where some kind of conflict or negative interaction has occurred
    between a police officer and a citizen ("officer-citizen conflict",
    or OCC).
    
    We do this by searching for either a phrase meaning a
    citizen wishes to file a complaint against an officer or both a
    phrase identifying an officer and a phrase indicating some kind
    of conflict (such as "rude", "upset", or "disrespect").  It's
    impossible to estimate the recall of this approach, as there are
    many different ways to describe an OCC in the call notes, but I
    would estimate that the precision is high enough to be useful
    (80-90%).  This flag could be best used to allow supervisors
    to keep an eye on any calls that negatively impact their
    relationship with the community (and possibly identify problem
    officers if there are repeat offenders), as opposed to providing
    a means of determining exactly how many OCC calls occur.

--- working_files/scripts/call_general_category.sql
+++ working_files/scripts/call_general_category.sql
@@ -49,6 +49,47 @@ mentally_ill_ids AS (
       upper(body) !~ '\(73\)'
     ) OR
     (upper(body) LIKE '%BAKER ACT%')
+),
+officer_citizen_conflict_ids AS (
+  SELECT distinct call_id
+  FROM note
+  -- Ignore EMS/FIRE messages because they won't be reporting conflict between PD and citizens.
+  WHERE upper(body) NOT LIKE '[EMD]%' AND upper(body) NOT LIKE '[FIRE]%' AND upper(body) NOT LIKE '[EMS]%' AND
+  -- Gnarly regex to attempt to locate comments that indicate officer-citizen conflict
+  -- We do this by searching for either a specific phrase indicating a complaint about an officer or both 1) a reference to an officer ("officer phrase") and 2) a phrase associated with some kind of conflict ("conflict phrase")
+  upper(body) ~
+  $$(?x)# <-- (?x) allows comments
+  (
+    # First check for sequences that indicate an officer phrase
+    (OFFICER|\mOFCR\M|\mPD\M|POLICE|(\m[A-D]\d{3}\M(?![)}])))  # that last gargbage is an attempt to match a unit expression in a way that doesn't catch comments by units (ex. "(A132) <comments>" or "{A143} <comments>")
+    # Some other unimportant text; note that we exclude periods and commas here so that the officer and conflict phrases occur near each other in the sentence structure
+    [^.,]*?
+    # Now check for conflict phrases
+    (UPSET|UNHAPPY|\mRUDE\M|RUDELY|\mCOMPLAIN(ING)?\M|DISRESPECT|AGGRESSIVE|UNPROFESSIONAL)
+  )
+  # We could also have the conflict phrase before the officer phrase
+  |
+  (
+    # Conflict phrases again
+    (UPSET|UNHAPPY|\mRUDE\M|RUDELY|\mCOMPLAIN(ING)?\M|DISRESPECT|AGGRESSIVE|UNPROFESSIONAL)
+    # Other unimportant text again, again excluding clause-breaking punctuation
+    [^,.]*?
+    # Officer phrase again
+    (OFFICER|\mOFCR\M|\mPD\M|POLICE|(\m[A-D]\d{3}\M))
+  )
+  # We could also have some more specific phrasing indicating a complaint about an officer.  We can't just use "complaint" as a conflict phrase, because
+  # this word is used in several different ways in the corpus (synonym for "caller", referring to something like a customer dispute, etc), so we'll only look
+  # for the following very specific wording.
+  |
+  (
+    # Complaint with a space
+    COMPLAINT\s
+    # Many different combinations of prepositions are possible
+    (OF|ON)\s(AN )?
+    # Officer phrases -- last sequence is again an attempt to capture a unit designation.  Since the context for this sequence is more limited than it was
+    # above, we don't need to worry about catching the (A132), {A132} stuff.
+    (OFFICER|\mOFCR\M|(\m[A-D]\d{3}\M))
+  )$$
 )
 SELECT
   call_id,
@@ -67,5 +108,9 @@ SELECT
   CASE
     WHEN call_id IN (SELECT * FROM mentally_ill_ids) THEN TRUE
     ELSE FALSE
-  END AS mental_illness_related
+  END AS mental_illness_related,
+  CASE
+    WHEN call_id IN (SELECT * FROM officer_citizen_conflict_ids) THEN TRUE
+    ELSE FALSE
+  END AS officer_citizen_conflict
 FROM call;
