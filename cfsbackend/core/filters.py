cfe9c74f7b02dc839caee48b290d491df6026336
Clinton Dreisbach <cdreisbach@rti.org>
2015-09-17 14:39:14 -0400

    Added response_time and unit to filters
    Both of these are new filter types. response_time uses durations to
    filter and unit looks to see if that unit was involved at all in the
    call.

--- cfsbackend/core/filters.py
+++ cfsbackend/core/filters.py
@@ -1,15 +1,54 @@
-from django_filters import FilterSet, DateFromToRangeFilter
-from .models import Call, Incident
+from django_filters import FilterSet, DateFromToRangeFilter, MethodFilter, RangeFilter
+from django_filters.fields import RangeField
+from django.db.models import Q
+from django import forms
+
+from .models import Call
+
+
+class DurationRangeField(RangeField):
+    def __init__(self, *args, **kwargs):
+        fields = (
+            forms.DurationField(),
+            forms.DurationField())
+        super().__init__(fields, *args, **kwargs)
+
+
+class DurationRangeFilter(RangeFilter):
+    field_class = DurationRangeField
+
+    def filter(self, qs, value):
+        if value:
+            if value.start is not None and value.stop is not None:
+                lookup = '%s__range' % self.name
+                return self.get_method(qs)(**{lookup: (value.start, value.stop)})
+            else:
+
+                if value.start is not None:
+                    qs = self.get_method(qs)(**{'%s__gte' % self.name: value.start})
+                if value.stop is not None:
+                    qs = self.get_method(qs)(**{'%s__lte' % self.name: value.stop})
+        return qs
 
 
 class CallFilter(FilterSet):
     time_received = DateFromToRangeFilter()
+    time_routed = DateFromToRangeFilter()
+    time_finished = DateFromToRangeFilter()
     time_closed = DateFromToRangeFilter()
+    response_time = DurationRangeFilter()
+    unit = MethodFilter(action='filter_unit')
 
     class Meta:
         model = Call
-        fields = ['time_received', 'time_routed', 'time_finished', 'time_closed', 'city', 'district', 'beat', 'sector',
-                  'call_source', 'nature', 'priority', 'close_code', 'zip_code']
+        fields = ['zip_code', 'district', 'sector', 'beat',
+                  'call_source', 'nature', 'priority', 'close_code',
+                  'primary_unit', 'first_dispatched', 'reporting_unit',
+                  ]
+
+    def filter_unit(self, queryset, value):
+        query = Q(primary_unit_id=value) | Q(first_dispatched_id=value) | Q(reporting_unit_id=value)
+        return queryset.filter(query)
 
 
 class SummaryFilter(FilterSet):
