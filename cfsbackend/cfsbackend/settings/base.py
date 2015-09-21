5faa4bc92f5aff8a5cc2d195b33be7fa6f8b1c6f
Clinton N. Dreisbach <crnixon@gmail.com>
2015-09-21 12:17:01 -0400

    Turn off timezone support
    This is not what we want in the long run, but for now, we can do it.
    All timestamps in the database are stored as local time.

--- cfsbackend/cfsbackend/settings/base.py
+++ cfsbackend/cfsbackend/settings/base.py
@@ -112,7 +112,9 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# May I be forgiven for this
+# USE_TZ = True
+USE_TZ = False
 
 # Not sure this is properly set up - even for dev testing
 # https://docs.djangoproject.com/en/1.8/topics/cache/
