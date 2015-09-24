0c5be0c57efeb3b996942c22ad2418238fcf4ad8
Adam Preston <adam.m.preston@gmail.com>
2015-09-24 14:26:10 -0600

    the plumbing for the call list view page

--- cfsbackend/core/views/frontend.py
+++ cfsbackend/core/views/frontend.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class DashboardView(TemplateView):
     template_name = "index.html"
+
+class CallListView(TemplateView):
+    template_name = "calls.html"
