1184ba0b0ec09fc8caa3393e4fed238e46c4e480
Adam Preston <adam.m.preston@gmail.com>
2015-09-24 14:26:10 -0600

    the plumbing for the call list view page

--- cfsbackend/core/views/frontend.py
+++ cfsbackend/core/views/frontend.py
@@ -4,8 +4,11 @@ from django.views.generic import View
 from ..filters import CallFilter
 from ..forms import JSONForm
 
-
 class DashboardView(View):
     def get(self, request, *args, **kwargs):
         return render_to_response("index.html",
                                   dict(form=JSONForm(CallFilter().form)))
+
+class CallListView(TemplateView):
+    template_name = "calls.html"
+
