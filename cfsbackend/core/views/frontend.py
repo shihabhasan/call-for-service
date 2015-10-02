a9d51aa3f23fb76477977d7938d9b4627bb91b09
Jason Nance <jnance@rti.org>
2015-10-02 14:41:26 -0400

    Add page with overview of modeling progress
    Contains some figures and an overview of our model, the data that
    drives it, and the results.  Needs proofreading and to be linked
    into the main dashboard somehow.

--- cfsbackend/core/views/frontend.py
+++ cfsbackend/core/views/frontend.py
@@ -4,14 +4,13 @@ from django.views.generic import View, TemplateView
 from ..filters import CallFilter
 from ..forms import JSONForm
 
-
 class CallListView(TemplateView):
-  def get(self, request, *args, **kwargs):
-        return render_to_response("calls.html",
-                                  dict(form=JSONForm(CallFilter().form)))
+    template_name = "calls.html"
 
 class DashboardView(View):
     def get(self, request, *args, **kwargs):
         return render_to_response("index.html",
                                   dict(form=JSONForm(CallFilter().form)))
 
+class PredictiveView(TemplateView):
+    template_name = "predictive.html"
