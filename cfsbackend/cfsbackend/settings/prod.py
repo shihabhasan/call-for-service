e682ae891db344e05c4bcc70645f9454e9558b1d
Clinton N. Dreisbach <cdreisbach@rti.org>
2015-10-06 17:13:36 -0400

    Gunicorn is working

--- cfsbackend/cfsbackend/settings/prod.py
+++ cfsbackend/cfsbackend/settings/prod.py
@@ -2,5 +2,23 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 COMPRESS_OFFLINE = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
