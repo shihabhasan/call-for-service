b498c75d12520de310c79b4ac6423d7045e6f9bb
Clinton N. Dreisbach <cdreisbach@rti.org>
2015-10-06 17:23:21 -0400

    Try leaving compression off

--- cfsbackend/cfsbackend/settings/prod.py
+++ cfsbackend/cfsbackend/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-COMPRESS_OFFLINE = True
+# COMPRESS_OFFLINE = True
 
 ALLOWED_HOSTS = ['159.203.84.81']
 
