8e31021fc56f3e9da3ce792d9c94418fdeeb8611
Clinton N. Dreisbach <cdreisbach@rti.org>
2015-10-07 09:42:26 -0400

    Add filtering by click on beat and nature

--- cfsbackend/core/static/dashboard/dashboard.js
+++ cfsbackend/core/static/dashboard/dashboard.js
@@ -9,10 +9,6 @@ var outFormats = {
     "hour": "%m/%d %H:%M"
 };
 
-// Have to keep track of this so we can reset it when the AJAX request
-// forces the scroll to the top of the page
-var curScroll = 0;
-
 var dashboard = new Ractive({
     el: document.getElementById("dashboard"),
     template: "#dashboard-template",
@@ -257,13 +253,11 @@ function buildVolumeByNatureChart(data) {
 
         svg.datum([{key: "Call Volume", values: data}]).call(chart);
 
-        /* Click filtering; too buggy for the client to see right now
         svg.selectAll('.nv-bar').style('cursor', 'pointer');
 
         chart.discretebar.dispatch.on('elementClick', function (e) {
             toggleFilter("nature", e.data.id);
         });
-        */
 
         // Have to call this both during creation and after updating the chart
         // when the window is resized.
@@ -393,12 +387,11 @@ function buildVolumeByBeatChart(data) {
 
         svg.datum(data).call(chart);
 
-        /* More click filtering
+        // More click filtering
         svg.selectAll('.nv-bar').style('cursor', 'pointer');
         chart.multibar.dispatch.on('elementClick', function (e) {
             toggleFilter("beat", e.data.id);
         });
-        */
 
         nv.utils.windowResize(chart.update);
 
