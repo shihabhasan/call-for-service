9249fd9646d89b93ea27764bde2d9d50836a8f73
Jason Nance <jnance@rti.org>
2015-11-09 17:56:27 -0500

    Add discrete_officer_activity view
    This materialized view brute forces the taxing component of the
    aggregation and should improve performance of the officer
    activity model.

--- working_files/scripts/officer_activity.sql
+++ working_files/scripts/officer_activity.sql
@@ -46,3 +46,33 @@ CREATE MATERIALIZED VIEW officer_activity AS
    WHERE
      sh.in_time IS NOT NULL AND
      sh.out_time IS NOT NULL) activity; 
+     
+/*
+This view has a row for each instance of officer activity at each 10 minute interval.  It can be used to aggregate activity up based on discrete time intervals instead of a continuous start_time to end_time.
+*/
+
+-- temp view needed here for speed
+DROP MATERIALIZED VIEW IF EXISTS time_sample CASCADE;
+CREATE MATERIALIZED VIEW time_sample AS
+SELECT * FROM
+generate_series('2014-01-01 00:00'::timestamp,
+                '2014-12-31 23:59'::timestamp,
+                '10 minutes') AS series(time_);
+
+CREATE UNIQUE INDEX time_sample_time
+  ON time_sample(time_); 
+
+DROP MATERIALIZED VIEW IF EXISTS discrete_officer_activity CASCADE;
+
+CREATE MATERIALIZED VIEW discrete_officer_activity AS
+  SELECT
+    ROW_NUMBER() OVER (ORDER BY start_time ASC) AS discrete_officer_activity_id,
+    ts.time_,
+    oa.call_unit_id,
+    oa.activity,
+    oa.call_id
+  FROM
+    officer_activity oa,
+    time_sample ts
+  WHERE
+    ts.time_ BETWEEN oa.start_time AND oa.end_time;
