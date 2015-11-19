06ac2668c5a415aa03ed6b09f565d46af3b0407b
Clinton N. Dreisbach <cdreisbach@rti.org>
2015-11-19 14:04:42 -0500

    Update production loggers

--- cfsbackend/cfsbackend/settings/prod.py
+++ cfsbackend/cfsbackend/settings/prod.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = ['159.203.84.81', 'dpdcfs.rticds.org']
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'handlers': {
         'file': {
             'level': 'DEBUG',
@@ -22,15 +22,10 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': True,
         },
-        'django.request': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
         'django.db.backends': {
             'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,
-        },
+        }
     },
 }
