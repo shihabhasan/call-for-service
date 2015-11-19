98d51eca97f9a73f7836c4b30ae8754d6072d9be
Clinton N. Dreisbach <cdreisbach@rti.org>
2015-11-19 14:36:51 -0500

    Install django-debug-toolbar

--- cfsbackend/cfsbackend/settings/local.py
+++ cfsbackend/cfsbackend/settings/local.py
@@ -6,8 +6,18 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+from fnmatch import fnmatch
+class glob_list(list):
+    def __contains__(self, key):
+        for elt in self:
+            if fnmatch(key, elt): return True
+        return False
+
+INTERNAL_IPS = glob_list(['127.0.0.1', '10.*.*.*'])
+
 INSTALLED_APPS += (
     'django_nose',
+    'debug_toolbar',
 )
 
 LOGGING = {
