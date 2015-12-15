0ec931f1f775e501873f3e3e58db0f20bfed53d2
Clinton N. Dreisbach <cdreisbach@rti.org>
2015-12-15 10:11:56 -0500

    Started call map API endpoint

--- cfsbackend/cfsbackend/urls.py
+++ cfsbackend/cfsbackend/urls.py
@@ -7,36 +7,19 @@ from rest_framework import routers
 from core import views
 
 router = routers.DefaultRouter()
-
-router.register(r'calls_sources', views.CallSourceViewSet)
-router.register(r'calls_units', views.CallUnitViewSet)
-router.register(r'nature', views.NatureViewSet)
-router.register(r'close_codes', views.CloseCodeViewSet)
-router.register(r'cities', views.CityViewSet)
-router.register(r'sectors', views.SectorViewSet)
-router.register(r'districts', views.DistrictViewSet)
-router.register(r'beats', views.BeatViewSet)
-
 router.register(r'calls', views.CallViewSet)
-
-# You must use the optional 3rd parameter here. or otherwise it changes the URL for calls.
-# the same holds true for any double-usage of the same queryset model in the ViewSet.
-router.register(r'calls_overview', views.CallOverviewViewSet, 'callsoverview')
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^api/call_volume/$', views.CallVolumeView.as_view()),
     url(r'^api/response_time/$', views.CallResponseTimeView.as_view()),
-    url(r'^api/map_info/$', views.MapInfoView.as_view()),
+    url(r'^api/call_map/$', views.CallMapView.as_view()),
     url(r'^api/officer_allocation/', views.OfficerAllocationView.as_view()),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
-    url(r'^call_volume$', views.DashboardView.as_view()),
+    url(r'^call_volume$', views.CallVolumeView.as_view()),
     url(r'^response_time$', views.ResponseTimeView.as_view()),
-    url(r'^calls$', views.CallListView.as_view()),
-    url(r'^predictive$', views.PredictiveView.as_view()),
-    url(r'^map$', views.MapView.as_view()),
     url(r'^$', views.DashboardView.as_view()),
     url(r'^officer_allocation$', views.OfficerAllocationDashboardView.as_view()),
 ]
