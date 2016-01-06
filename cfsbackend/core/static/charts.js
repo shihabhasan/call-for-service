0551a38119481f9a2be6329a938b1c8161a147d1
Clinton N. Dreisbach <cdreisbach@rti.org>
2016-01-06 12:55:49 -0500

    Update heatmap to put times between blocks

--- cfsbackend/core/static/charts.js
+++ cfsbackend/core/static/charts.js
@@ -13,9 +13,9 @@ var Heatmap = function(options) {
   };
 
   var days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-      times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a",
-        "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p",
-        "9p", "10p", "11p", "12p",
+      times = ["12a", "1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a",
+        "11a", "12p", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p",
+        "9p", "10p", "11p", "12a"
       ];
 
   this.el = options.el;
@@ -108,7 +108,7 @@ var Heatmap = function(options) {
           })
       .attr("y", gridSize * 1.5)
       .style("text-anchor", "middle")
-      .attr("transform", "translate(" + (gridSize / 2) + ",-8)")
+      .attr("transform", "translate(0,-8)")
       .attr("class", "timeLabel axis");
   };
 
