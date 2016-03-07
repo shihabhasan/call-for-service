74a91f6db043c3dce04967f28f30bf347c23a2c0
Jason Nance <jnance@rti.org>
2016-03-07 16:10:07 -0500

    Make ETL idempotent, optionally clear database
    As a first step to incrementally loading the database, we need to make
    sure the script can handle seeing data it already has (avoiding a bunch
    of unique/PK constraint violations).  It's now able to re-load a dataset
    without errors, and it doesn't just clear the DB every time.

--- cfsbackend/core/management/commands/importcfsdata.py
+++ cfsbackend/core/management/commands/importcfsdata.py
@@ -7,8 +7,12 @@ class Command(BaseCommand):
     help = "Load CFS data."
 
     def add_arguments(self, parser):
-        parser.add_argument('dir', nargs=1, type=str)
+        parser.add_argument('dir', type=str, help='Directory containing '
+                'the files to load')
+        parser.add_argument('--reset', type=bool, default=False,
+                help='Whether to clear the database before loading '
+                '(defaults to False)')
 
     def handle(self, *args, **options):
-        etl = ETL(dir=options['dir'][0])
+        etl = ETL(dir=options['dir'], reset=options['reset'])
         etl.run()
