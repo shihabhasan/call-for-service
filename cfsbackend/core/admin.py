d89d109c91993fcfae665ff7ace840dfce6a2057
Mae Beale <mbeale@rti.org>
2016-06-20 14:22:52 -0400

    Complete admin site customization
    - Alphabetize inline classes separately from model admin classes
    - Add custom list display for all classes with char fields
    - Add inlines for all classes with child objects
    - Add TextInput override so they show as a single line
    - Add classes: InCallPeriodAdmin, OutOfServicePeriodInline,
      CallUnitInline

--- cfsbackend/core/admin.py
+++ cfsbackend/core/admin.py
@@ -1,25 +1,98 @@
 from django.db import models
 from django.forms import TextInput
 from django.contrib import admin
-from .models import  Beat, District, Nature, NatureGroup, Bureau, CallSource, \
-    CallUnit, ZipCode, City, Priority, CloseCode, Division, Officer, \
-    OfficerActivity, OfficerActivityType, OOSCode, OutOfServicePeriod, Shift, Squad, \
-    Transaction, \
-    Unit
+from .models import  Beat, Bureau, CallSource, CallUnit, City, CloseCode, \
+    District, Division, InCallPeriod, Nature, NatureGroup,  \
+    Officer, OfficerActivity, OfficerActivityType, OOSCode, OutOfServicePeriod, \
+    Priority, Shift, ShiftUnit, Squad, Transaction, Unit, ZipCode
 
+### model inline classes
 
-# Register your models here.
-
-class BeatInline(admin.StackedInline):
+class BeatInline(admin.TabularInline):
     model = Beat
+    extra = 0
     exclude = ('sector',)
     formfield_overrides = {
         models.TextField: {'widget': TextInput}
     }
 
 
+class CallUnitInline(admin.TabularInline):
+    model = CallUnit
+    extra = 0
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+class NatureInline(admin.StackedInline):
+    model = Nature
+    extra = 0
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+class OutOfServicePeriodInline(admin.TabularInline):
+    model = OutOfServicePeriod
+    extra = 0
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+class ShiftUnitInline(admin.TabularInline):
+    model = ShiftUnit
+    extra = 0
+
+
+### model admin classes
+
 @admin.register(Beat)
 class BeatAdmin(admin.ModelAdmin):
+    list_display = ('descr', 'district',)
+    inlines = [CallUnitInline]
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+@admin.register(Bureau)
+class BureauAdmin(admin.ModelAdmin):
+    list_display = ('descr', 'code',)
+    inlines = [ShiftUnitInline]
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+@admin.register(CallSource)
+class CallSourceAdmin(admin.ModelAdmin):
+    list_display = ('descr', 'code',)
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+@admin.register(CallUnit)
+class CallUnitAdmin(admin.ModelAdmin):
+    list_display = ('descr', 'squad', 'beat', 'district',)
+    inlines = [ShiftUnitInline]
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+@admin.register(CloseCode)
+class CloseCodeAdmin(admin.ModelAdmin):
+    list_display = ('descr', 'code',)
     formfield_overrides = {
         models.TextField: {'widget': TextInput}
     }
@@ -27,7 +100,17 @@ class BeatAdmin(admin.ModelAdmin):
 
 @admin.register(District)
 class DistrictAdmin(admin.ModelAdmin):
-    inlines = [BeatInline]
+    exclude = ('sector',)
+    inlines = [BeatInline, CallUnitInline]
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput(attrs={'size':'50'})}
+    }
+
+
+@admin.register(Division)
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = ('descr', 'code',)
+    inlines = [ShiftUnitInline]
     formfield_overrides = {
         models.TextField: {'widget': TextInput}
     }
@@ -35,12 +118,6 @@ class DistrictAdmin(admin.ModelAdmin):
 
 @admin.register(Nature)
 class NatureAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.TextField: {'widget': TextInput}
-    }
-
-class NatureInline(admin.StackedInline):
-    model = Nature
     formfield_overrides = {
         models.TextField: {'widget': TextInput}
     }
@@ -54,39 +131,10 @@ class NatureGroupAdmin(admin.ModelAdmin):
     }
 
 
-@admin.register(Bureau)
-class BureauAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(CallSource)
-class CallSourceAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(CallUnit)
-class CallUnitAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(City)
-class CityAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(CloseCode)
-class CloseCodeAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Division)
-class DivisionAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(Officer)
 class OfficerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'name_aka',)
+    inlines = [ShiftUnitInline]
 
 
 @admin.register(OfficerActivity)
@@ -96,44 +144,74 @@ class OfficerActivityAdmin(admin.ModelAdmin):
 
 @admin.register(OfficerActivityType)
 class OfficerActivityTypeAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
 
 
 @admin.register(OOSCode)
 class OOSCodeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('descr', 'code',)
+    inlines = [OutOfServicePeriodInline]
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
 
 
 @admin.register(OutOfServicePeriod)
 class OutOfServicePeriodAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
+
+
+@admin.register(InCallPeriod)
+class InCallPeriodAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
 
 
 @admin.register(Priority)
 class PriorityAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
 
 
 @admin.register(Shift)
 class ShiftAdmin(admin.ModelAdmin):
+    inlines = [ShiftUnitInline]
+
+
+@admin.register(ShiftUnit)
+class ShiftUnitAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Squad)
 class SquadAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
 
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('code',)
 
 
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('descr', 'code',)
+    inlines = [ShiftUnitInline]
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
 
 
 @admin.register(ZipCode)
 class ZipCodeAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput}
+    }
