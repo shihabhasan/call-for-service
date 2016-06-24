5d41e7ae7d235ae0fb3f160d89db46f7de82b7be
Clinton Dreisbach <cdreisbach@rti.org>
2016-06-24 12:20:46 -0400

    Move officer allocation views into app

--- cfsbackend/core/migrations/0011_add_indexes.py
+++ cfsbackend/core/migrations/0011_add_indexes.py
@@ -6,15 +6,6 @@ import os, os.path
 base_dir = os.path.realpath(os.path.dirname(__file__))
 
 
-def sql_path(filename):
-    return os.path.join(base_dir, "sql", filename)
-
-
-with open(sql_path("in_call.sql")) as file:
-    in_call_sql = file.read()
-with open(sql_path("officer_activity.sql")) as file:
-    officer_activity_sql = file.read()
-
 timestamp_cols = [
     ('call', 'time_received'),
     ('call', 'time_routed'),
--- cfsbackend/officer_allocation/migrations/0002_create_views.py
+++ cfsbackend/officer_allocation/migrations/0002_create_views.py
@@ -9,15 +9,19 @@ base_dir = os.path.realpath(os.path.dirname(__file__))
 def sql_path(filename):
     return os.path.join(base_dir, "sql", filename)
 
-with open(sql_path("call_general_category.sql")) as file:
-    call_general_category_sql = file.read()
 
+with open(sql_path("in_call.sql")) as file:
+    in_call_sql = file.read()
+
+with open(sql_path("officer_activity.sql")) as file:
+    officer_activity_sql = file.read()
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('core', '0008_auto_20151130_2311'),
+        ('officer_allocation', '0001_initial'),
     ]
 
     operations = [
-        migrations.RunSQL(call_general_category_sql),
+        migrations.RunSQL(in_call_sql),
+        migrations.RunSQL(officer_activity_sql)
     ]
