797d782c5fd006f0284626b6d0d104eaad471a97
Clinton Dreisbach <cdreisbach@rti.org>
2016-06-27 11:00:57 -0400

    Fix migration 0016

--- cfsbackend/core/migrations/0016_auto_20160621_1553.py
+++ cfsbackend/core/migrations/0016_auto_20160621_1553.py
@@ -15,26 +15,14 @@ class Migration(migrations.Migration):
             name='OfficerActivityType',
         ),
         migrations.DeleteModel(
+            name='OutOfServicePeriod',
+        ),
+        migrations.DeleteModel(
             name='OOSCode',
-        ),
-        migrations.RemoveField(
-            model_name='outofserviceperiod',
-            name='call_unit',
-        ),
-        migrations.RemoveField(
-            model_name='outofserviceperiod',
-            name='oos_code',
-        ),
-        migrations.RemoveField(
-            model_name='outofserviceperiod',
-            name='shift',
         ),
         migrations.AlterField(
             model_name='siteconfiguration',
             name='department_abbr',
             field=models.CharField(default='CPD', verbose_name='Department abbreviation', max_length=10),
-        ),
-        migrations.DeleteModel(
-            name='OutOfServicePeriod',
         ),
     ]
